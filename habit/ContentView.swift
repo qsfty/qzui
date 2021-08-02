4c764af49416a28b5dce456550f335049c371cc4
minqiang.li <minqiang.li@tongdun.net>
2021-08-02 16:15:23 +0800

    Initial Commit

---
+++ habit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  habit
+//
+//  Created by 李和 on 2021/8/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ habit/habitApp.swift
@@ -0,0 +1,17 @@
+//
+//  habitApp.swift
+//  habit
+//
+//  Created by 李和 on 2021/8/2.
+//
+
+import SwiftUI
+
+@main
+struct habitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
